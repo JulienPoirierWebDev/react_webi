c88af227ac65fb80e338276b7528a68187e7c7bb
Julien POIRIER <connexion@julienpoirier-Webdev.com>
2024-12-10 13:58:53 +0100

    search with input

--- src/components/MoviesSearch.jsx
+++ src/components/MoviesSearch.jsx
@@ -1,30 +1,44 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import Movie from './Movie/Movie';
+import InputContainer from './InputContainer/InputContainer.jsx';
 
 const MoviesSearch = () => {
 	const [movies, setMovies] = useState([]);
+	const inputRef = useRef(null);
 
-	useEffect(() => {
-		async function getMovies() {
-			const response = await fetch(
-				'http://localhost:3000/query-movies/avengers'
-			);
+	async function getMovies(searchTerm) {
+		const response = await fetch(
+			`http://localhost:3000/query-movies/${searchTerm}`
+		);
 
-			const data = await response.json();
+		const data = await response.json();
 
-			console.log(data.data.results);
+		setMovies(data.data.results);
+	}
 
-			setMovies(data.data.results);
-		}
+	const handleSubmit = (event) => {
+		event.preventDefault();
 
-		getMovies();
+		//console.log(event.target.children[0].children[1].value);
+		//console.log(inputRef.current.value);
+		const searchTerm = inputRef.current.value;
+		getMovies(searchTerm);
+	};
 
-		return () => {
-			console.log('Mon composant APP est démonté');
-		};
-	}, []);
 	return (
 		<div>
+			<form action='' onSubmit={handleSubmit}>
+				<InputContainer>
+					<label htmlFor='search'>Rechercher un film</label>
+					<input
+						ref={inputRef}
+						type='text'
+						id='search'
+						name='search'
+					/>
+				</InputContainer>
+				<input type='submit' value='Rechercher' />
+			</form>
 			{movies.map((movie) => {
 				return <Movie key={movie.id} movie={movie} />;
 			})}
